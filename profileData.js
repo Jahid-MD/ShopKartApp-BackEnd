10dbeabd44092c6c71e9e3bf6ad1e0ca8e74ff2c
jahid MD <jahidmohd017@gmail.com>
2021-12-29 17:25:16 +0530

    after json formming

---
+++ profileData.js
@@ -0,0 +1,11 @@
+profileData = {
+    user1: {
+        id: "user1",
+        userName: "username",
+        email: "abc@xyz.com",
+        img: "./",
+        phoneNumber: "+91 8974421215",
+        password: "12312121",
+    },
+    keys: [user1],
+};
